ff0996701bc3ef2ce6cf6737ed639f1a82a8f870
Irene da Cruz <86837799+Iredcg@users.noreply.github.com>
2024-02-15 11:39:20 +0100

    Add files via upload

---
+++ test_24.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+# In[3]:
+
+
+st.title("_This_is our first website")
+
+
+# In[ ]:
+
+st.write("This site is beautiful")
+
+peng_df=sns.load_dataset("penguins")
+
+with st.expander("Show me the penguins"):
+    st.dataframe(peng_df)
+
+col1, col2, col3 = st.columns(3)   
+
+option= st.selectbox("Which penguin do you want?", 
+                    ("Adelie", "Chinstrap", "Gentoo"))
+
+if option == "Adelie":
+    with col1:
+        st.header("Adelie")
+        st.image("Images/Images/Adelie.png")
+
+if option == "Chinstrap":
+    with col2:
+        st.header("Chinstrap")
+        st.image("Images/Images/Chinstrap.png")
+
+if option == "Gentoo":
+    with col3:
+        st.header("Gentoo")
+        st.image("Images/Images/Gentoo.png")
+
+    
+    
